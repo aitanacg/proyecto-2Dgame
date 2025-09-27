6ab29aaf540f764d27122a81e1edcd1a512a7be3
aitanacg <cavogassen@gmail.com>
2025-09-27 14:56:23 +0200

    feat: added collision

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
@@ -12,18 +13,30 @@ public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+    public final int screenX;
+    public final int screenY;
+
     public Player(GamePanel gp, KeyHandler keyH) {
 
         this.gp = gp;
         this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
+
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidArea.width = 32;
+        solidArea.height = 32;
 
         setDefaultValues();
         getPlayerImage();
     }
 
     public void setDefaultValues() {
-        x = 100;
-        y = 100;
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
         speed = 4;
         direction = "down";
     }
@@ -80,17 +93,35 @@ public class Player extends Entity {
                 keyH.leftPressed == true || keyH.rightPressed == true) {
 
             if (keyH.upPressed == true) {
-                direction = "up";
-                y -= speed;
+                direction = "up";              
             } else if (keyH.downPressed == true) {
-                direction = "down";
-                y += speed;
+                direction = "down";               
             } else if (keyH.leftPressed == true) {
                 direction = "left";
-                x -= speed;
             } else if (keyH.rightPressed == true) {
-                direction = "right";
-                x += speed;
+                direction = "right";               
+            }
+            
+            // CHECK TILE COLLISION
+            collisionOn = false;
+            gp.cChecker.checkTile(this);
+
+            // IF COLLISION IS FALSE< PLAYER CAN MOVE
+            if (collisionOn == false) {
+                switch (direction) {
+                    case "up":
+                        worldY -= speed;
+                        break;
+                    case "down":
+                        worldY += speed;
+                        break;
+                    case "left":
+                        worldX -= speed;
+                        break;
+                    case "right":
+                        worldX += speed;
+                        break;
+                }
             }
 
             spriteCounter++;
@@ -145,7 +176,7 @@ public class Player extends Entity {
                 break;
 
         }
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
     }
 
 }
